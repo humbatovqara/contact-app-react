724f80e3ad1e08440e5c657f1875401a0a2edf1a
Qara <qarahumbtov@gmail.com>
2022-03-24 17:31:57 +0400

    Add Form, Filter, Style components

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import "./styles.css"
+import { useState, useEffect } from 'react'
 import List from './List'
 import Form from './Form'
 
 function Contacts() {
+    const [contacts, setContacts] = useState([
+      {
+        fullname: "Qara",
+        phone_number: "055"
+      },
+      {
+        fullname: "Ulvi",
+        phone_number: "051"
+      },
+      {
+        fullname: "Nurlan",
+        phone_number: "077"
+      }
+    ]);
+
+    useEffect(() => {
+        console.log(contacts);
+    }, [contacts])
+
   return (
-    <div>
-        <List />
-        <Form />
+    <div id='container'>
+      <h1>Contacts</h1>
+        <List contacts={contacts} />
+        <Form addContact={setContacts} contacts={contacts} />
     </div>
   )
 }
